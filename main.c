2a5356d05392aae2b60c3bbdea595edf8d817771
Simba Ndoro <simbandoro.sn@gmail.com>
2019-04-04 05:12:52 +0000

    changed "cipher" to "alphabet"

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 
 int main()  {
 
-    char cipher[26]; // this indicates that there are 26 characters type variables (so the alphabet from A to Z) 
+    char alphabet[26]; // this indicates that there are 26 characters type variables (so the alphabet from A to Z) 
     int shift; //this is to declare integer variable of type shift. 
                 //This is how many letters the encryption will be shifted by
+                
              
              
   
