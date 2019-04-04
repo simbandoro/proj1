232413cef6943fedb3dfc0d2bcd62013479a0b95
Simba Ndoro <simbandoro.sn@gmail.com>
2019-04-04 04:56:58 +0000

    declared the cipher variable as "char" for characters declared the shift variable as "int" because the encryption will be shifted by integer values

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 
 int main()  {
 
-    
+    char cipher[26]; // this indicates that there are 26 characters type variables (so the alphabet from A to Z) 
+    int shift; //this is to declare integer variable of type shift. 
+                //This is how many letters the encryption will be shifted by
+             
+             
+  
+  
+  
+  
+  
+  
   return 0;
   }
