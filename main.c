e406e1e2eaeadf8f68651344aa78697950e51fd1
Simba Ndoro <simabndoro.sn@gmail.com>
2019-04-11 04:44:34 +0000

    added that encrypt message part

--- main.c
+++ main.c
@@ -6,6 +6,10 @@ int main()  {
     int shift; //this is to declare integer variable of type shift. 
                 //This is how many letters the encryption will be shifted by
                 
+        printf("Enter a message to encrypt: "); // yeah this is here 
+        
+        gets(alphabet);     //this is telling the computer to scan the 26 letters of the alphabet
+        
         
   
   return 0;
