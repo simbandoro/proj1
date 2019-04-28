f7f7633430516f5ed4a04076e6a9947a45c90d61
simbandoro <48078818+simbandoro@users.noreply.github.com>
2019-04-28 21:48:05 +1000

    Update main.c
    Added substitution cipher that actually works

--- main.c
+++ main.c
@@ -91,13 +91,43 @@ void upperCase(char str[]) {
   scanf ("%d", &shift); // this is to scan the rotation amount
 
   for (i = 0; (i < 500 && str[i] != '\0'); i++)
-    str[i] = (str[i] - shift);	/*the key for encryption is shift for me.
+    if(str[i] > 64 && str[i] < 123) {
+	  str[i] = (str[i] - shift);	/*the key for encryption is shift for me.
 				   this will be determined by the user 
 				   this is added to ASCII value */
+    }
 	
   upperCase(str); // call the uppercase function so that the output is in uppercase	
 
   printf ("DECRYPTED MESSAGE: %s\n", str);
   
   return 0;
+	
+	/******** SUBSTITUTION ENCRYPTION BELOW ***************/
+		
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i;  // for counter 
+    char location_string;   //this is the position of a character 
+    char str[200]; 
+    
+    char SubKey[26] = {"QWERTYUIOPASDFGHJKLZXCVBNM"}; //this is the substitution key
+    
+    printf("Enter message:\n");
+    fgets(str,200,stdin);
+    
+    for (i=0; str[i] != '\0'; i++){
+            if(str[i] > 64 && str[i] < 123){   //this is to ignore non-alphabet characters
+                str[i] = SubKey[str[i]-65]; //this is to give the position of the substitution key character within the array
+            }
+        }
+    upperCase(str); // if input is lowercase it converts the output to uppercase 
+    printf("\nSubstitution Cipher: %s\n", str); //prints the substitution key character at that position
+    
+    return 0;
+}
+
   
