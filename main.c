91cdb347158a7fc563e41df34174623c5b3d9393
simbandoro <48078818+simbandoro@users.noreply.github.com>
2019-04-29 02:55:45 +1000

    Update main.c
    Neatening up

--- main.c
+++ main.c
@@ -1,31 +1,5 @@
 #include <stdio.h>
 #include <string.h>     // for string length function
-
-int main()  {
-   
-    char alphabet[26]; // this indicates that there are 26 characters type variables (so the alphabet from A to Z) 
-    int shift; //this is to declare integer variable of type shift. 
-                //This is how many letters the encryption will be shifted by
-    int i;
-   
-                
-        printf("Enter a message to encrypt: "); // yeah this is here 
-        
-        gets(alphabet);     //this is telling the computer to scan the 26 letters of the alphabet
-        
-   /* the for loop here is gonna be for the incrementation */
-   
-   for(i = 0; alphabet[i] != '\0'; i++)  {
-        alphabet = alphabet [i];
-        
-        if (alphabet >= 'a' && alphabet <= 'z') {
-            alphabet = alphabet + shift;
-            
-                if (alphabet > 'z'){
-                    
-                }
-        }
-    }
   
     /******** ROTATION CIPHER BELOW ********
     the cipher has a problem when letters are shifted past alphabet variables 
@@ -36,7 +10,7 @@ void upperCase(char []); //declaring function to convert output to upper case
 int main()  {
 
     int i;
-  char str[500];
+  char str[500];	
   int shift; // how much each character is shifted by (rotation amount)
 
   printf ("\nENTER MESSAGE:\t");	// \t prints a tab
