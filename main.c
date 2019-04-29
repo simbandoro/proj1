3edc293c96f8bee63bb8af100012aad34ea35e06
simbandoro <48078818+simbandoro@users.noreply.github.com>
2019-04-29 15:18:00 +1000

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ main ()
   int shift;			// how much each character is shifted by (rotation amount)
   int x;			//another counter used in substitution decryption
 
-  char str[500];
+  char str[500];    //stores 500 characters
 
   char SubKey[26] = { "QWERTYUIOPASDFGHJKLZXCVBNM" };	//this is the substitution key
 
@@ -22,13 +22,11 @@ main ()
   printf ("4 = Substitution cipher decryption\n");
   scanf ("%d", &num);
 
-  switch (num)
+  switch (num)  //task determination
     {
     case 1:
       printf ("\nENTER MESSAGE:\t");	// \t prints a tab
       scanf ("%s", str);
-      /*fgets (str, 500, stdin);     This is to get the string from the keyboard. 
-         fgets is safer to use than gets */
       printf ("%s", str);
 
       printf ("\nROTATION AMOUNT:\t");
@@ -42,11 +40,13 @@ main ()
 					   this is added to ASCII value */
       if (str[i] < 65)
 	{
-	  str[i] = str[i] + 26;
+	  str[i] = str[i] + 26;    //to get alphabet position if shifted beyond amount
+	  i++;
 	}
       else if (str[i] > 90)
 	{
-	  str[i] = str[i] - 26;
+	  str[i] = str[i] - 26;    //to get alphabet position if shifted beyond amount
+	  i++;
 	}
 
       upperCase (str);		// call the uppercase function so that the output is in uppercase
@@ -57,15 +57,15 @@ main ()
     case 2:
       printf ("\nENTER CIPHER TEXT:\t");
       scanf ("%s", str);
-      printf ("\n%s", str);
-      //fgets (str, 500, stdin);    
+      printf ("\n%s", str);    
 
       printf ("\nROTATION AMOUNT:\t");
       scanf ("%d", &shift);
       printf ("%d", shift);
 
       for (i = 0; (i < 500 && str[i] != '\0'); i++)
-	str[i] = (str[i] - shift);
+	  if (str[i] != 32)
+	  str[i] = (str[i] - shift);
 
 
       upperCase (str);
@@ -77,13 +77,12 @@ main ()
       printf ("\nEnter message:\n");
       scanf ("%s", str);
       printf ("%s\n", str);
-      //fgets(str,500,stdin);
 
       for (i = 0; str[i] != '\0'; i++)
 	{
-	  if (str[i] != 32)
-	    {
-	      //if(str[i] > 64 && str[i] < 123){   //this is to ignore non-alphabet characters
+	  //if (str[i] != 32)
+	      if(str[i] > 64 && str[i] < 123)
+	      {   //this is to ignore non-alphabet characters
 	      str[i] = SubKey[str[i] - 65];	//this is to give the position of the substitution key character within the array
 	    }
 	}
@@ -103,15 +102,16 @@ main ()
 	{
 	  for (x = 0; x <= 26; x++)
 	    {
-	      if (str[i] == SubKey[x])
+	      if (str[i] == SubKey[x]) /* The location of the letter in SubKey
+	                                  should = location in string */
 		{
 		  if ((str[i] <= 122) && (str[i] >= 97))
 		    {
-		      str[i] = str[i] - 32;
+		      str[i] = str[i] - 32; //Converting to uppercase
 		    }
 		  if ((str[i] <= 90) && (str[i] >= 65))
 		    {
-		      str[i] = x + 65;
+		      str[i] = x + 65; //To get the ASCII of an uppercase value
 		      //printf("%c", str[i]);
 		    }
 		}
